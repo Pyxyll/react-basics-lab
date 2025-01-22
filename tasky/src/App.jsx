6d314be4d4a465cfb5c364628e74e47f2f931634
Dylan Collins <pyxyllv@gmail.com>
2025-01-22 11:24:46 +0000

    Completed Tasky Lab 1

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,36 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Task from './components/task';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Tailwind, please no semantic ui</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          <code>hello world
-          </code>
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1>Tasky</h1>
+      <Task title="Dishes" deadline="Today" description="Wash and put away dishes"/>
+      <Task title="Laundry" deadline="Tomorrow" description="Wash and fold laundry"/>
+      <Task title="Tidy" deadline="Today" description="Clean the kitchen and sitting room" />	
+    </div>
+  );
 }
 
-export default App
+export default App;
